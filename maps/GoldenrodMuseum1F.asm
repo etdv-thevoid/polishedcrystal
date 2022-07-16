666d2958e519379c276706c6eeeb8afb11e162b5
The Void <9028176+etdv-thevoid@users.noreply.github.com>
2022-07-16 10:13:50 -0500

    Add NPCs and event checks to paintings

--- maps/GoldenrodMuseum1F.asm
+++ maps/GoldenrodMuseum1F.asm
@@ -15,3 +15,36 @@ GoldenrodMuseum1F_MapScriptHeader:
 	def_object_events
 	object_event  1,  2, SPRITE_SLOWPOKETAIL, SPRITEMOVEDATA_ARCH_TREE_DOWN, 0, 0, -1, -1, PAL_NPC_TREE, OBJECTTYPE_COMMAND, end, NULL, -1
 	object_event  2,  2, SPRITE_SLOWPOKETAIL, SPRITEMOVEDATA_ARCH_TREE_UP, 0, 0, -1, -1, PAL_NPC_TREE, OBJECTTYPE_COMMAND, end, NULL, -1
+	object_event  3,  3, SPRITE_RECEPTIONIST, SPRITEMOVEDATA_STANDING_DOWN, 0, 0, -1, -1, PAL_NPC_GREEN, OBJECTTYPE_COMMAND, jumptextfaceplayer, GoldenrodMuseumReceptionistText, -1
+	object_event  4,  4, SPRITE_OFFICER, SPRITEMOVEDATA_STANDING_RIGHT, 0, 0, -1, -1, PAL_NPC_BLUE, OBJECTTYPE_COMMAND, jumptextfaceplayer, GoldenrodMuseumOfficerText, -1
+	
+
+GoldenrodMuseumReceptionistText:
+	text "Welcome to the"
+	line "Goldenrod City"
+	cont "Museum!"
+
+	para "In celebration of"
+	line "our grand opening,"
+	
+	para "admission is free"
+	line "of charge."
+
+	para "Please, feel free"
+	line "to look around."
+
+	para "And we hope you"
+	line "enjoy your visit."
+	done
+
+GoldenrodMuseumOfficerText:
+	text "I'm keeping my eye"
+	line "on you kid!"
+
+	para "Hahaha! Just"
+	line "messing with ya!"
+
+	para "I'm keeping an eye"
+	line "on everyone."
+	done
+
--- maps/GoldenrodMuseum2F.asm
+++ maps/GoldenrodMuseum2F.asm
@@ -25,9 +25,15 @@ GoldenrodMuseum2F_MapScriptHeader:
 	bg_event  3,  7, BGEVENT_READ, HoOhPuzzleScript
 
 	def_object_events
+	object_event  4,  2, SPRITE_SIGHTSEER_M, SPRITEMOVEDATA_STANDING_UP, 0, 0, -1, (1 << EVE) | (1 << NITE), 0, OBJECTTYPE_SCRIPT, 0, GoldenrodMuseum2FSightseerMScript, -1
+	pokemon_event 5,  2, SMEARGLE, SPRITEMOVEDATA_POKEMON, -1, (1 << EVE) | (1 << NITE), PAL_NPC_BROWN, GoldenrodMuseum2FSmeargleText, -1
+	object_event  1,  5, SPRITE_SCIENTIST, SPRITEMOVEDATA_STANDING_LEFT, 0, 0, -1, -1, PAL_NPC_BROWN, OBJECTTYPE_COMMAND, jumptextfaceplayer, GoldenrodMuseum2FScientistText, -1
 	object_event  6,  3, SPRITE_BIG_LAPRAS, SPRITEMOVEDATA_STANDING_LEFT, 0, 0, -1, -1, PAL_NPC_ROCK, OBJECTTYPE_COMMAND, jumptext, GoldenrodMuseum2FBigPearlText, -1
+	object_event  6,  4, SPRITE_ROCKET, SPRITEMOVEDATA_STANDING_UP, 0, 0, -1, -1, 0, OBJECTTYPE_SCRIPT, 0, GoldenrodMuseum2FRocketScoutScript, EVENT_GOLDENROD_CITY_ROCKET_SCOUT
 
 LugiaPaintingScript:
+	checkevent EVENT_FOUGHT_LUGIA
+	iffalse_jumptext EmptyDisplayText
 	refreshscreen
 	paintingpic LUGIA_PAINTING
 	waitbutton
@@ -39,6 +45,8 @@ LugiaPaintingScript:
 	done
 
 HoOhPaintingScript:
+	checkevent EVENT_FOUGHT_HO_OH
+	iffalse_jumptext EmptyDisplayText
 	refreshscreen
 	paintingpic HO_OH_PAINTING
 	waitbutton
@@ -50,6 +58,8 @@ HoOhPaintingScript:
 	done
 
 BellTowerPaintingScript:
+	checkevent EVENT_FOUGHT_SUICUNE
+	iffalse_jumptext EmptyDisplayText
 	refreshscreen
 	paintingpic BELL_TOWER_PAINTING
 	waitbutton
@@ -62,7 +72,7 @@ BellTowerPaintingScript:
 
 KabutoPuzzleScript:
 	checkevent EVENT_SOLVED_KABUTO_PUZZLE
-	iffalse_jumptext UnsolvedPuzzleText
+	iffalse_jumptext EmptyDisplayText
 	refreshscreen
 	paintingpic KABUTO_PUZZLE
 	waitbutton
@@ -75,7 +85,7 @@ KabutoPuzzleScript:
 
 OmanytePuzzleScript:
 	checkevent EVENT_SOLVED_OMANYTE_PUZZLE
-	iffalse_jumptext UnsolvedPuzzleText
+	iffalse_jumptext EmptyDisplayText
 	refreshscreen
 	paintingpic OMANYTE_PUZZLE
 	waitbutton
@@ -88,7 +98,7 @@ OmanytePuzzleScript:
 
 AerodactylPuzzleScript:
 	checkevent EVENT_SOLVED_AERODACTYL_PUZZLE
-	iffalse_jumptext UnsolvedPuzzleText
+	iffalse_jumptext EmptyDisplayText
 	refreshscreen
 	paintingpic AERODACTYL_PUZZLE
 	waitbutton
@@ -101,7 +111,7 @@ AerodactylPuzzleScript:
 
 HoOhPuzzleScript:
 	checkevent EVENT_SOLVED_HO_OH_PUZZLE
-	iffalse_jumptext UnsolvedPuzzleText
+	iffalse_jumptext EmptyDisplayText
 	refreshscreen
 	paintingpic HO_OH_PUZZLE
 	waitbutton
@@ -112,13 +122,91 @@ HoOhPuzzleScript:
 	line "Ho-Oh panel!"
 	done
 
-UnsolvedPuzzleText:
+EmptyDisplayText:
 	text "The display is"
 	line "still empty…"
+	done
+
+GoldenrodMuseum2FScientistText:
+	text "These artifacts"
+	line "are fascinating!"
+	
+	para "Its a shame they're"
+	line "just replicas."
+
+	para "I'd love to study"
+	line "the real thing"
+	cont "up close."
 	done
 
 GoldenrodMuseum2FBigPearlText:
 	text "It's a huge pearl"
 	line "fished up from"
 	cont "Goldenrod Harbor."
+	done
+
+GoldenrodMuseum2FRocketScoutScript:
+	opentext
+	writetext GoldenrodMuseum2FRocketScoutText1
+	promptbutton
+	faceplayer
+	writetext GoldenrodMuseum2FRocketScoutText2
+	waitbutton
+	closetext
+	turnobject LAST_TALKED, UP
+	end
+
+GoldenrodMuseum2FRocketScoutText1:
+	text "Hmm… This would"
+	line "fetch quite a"
+	cont "heavy price."
+
+	para "Now… how do I get"
+	line "it out of here"
+
+	para "without getting"
+	line "caught?"
+	done
+
+GoldenrodMuseum2FRocketScoutText2:
+	text "Don't you know it's"
+	line "rude to stare?"
+	cont "Beat it kid!"
+	done
+
+GoldenrodMuseum2FSightseerMScript:
+	checkevent EVENT_FOUGHT_SUICUNE
+	iffalse_jumptextfaceplayer GoldenrodMuseum2FSightseerMNoPaintingText
+	jumpthistextfaceplayer
+
+	text "Wow! This painting"
+	line "is beautiful!"
+
+	para "One day, I hope my"
+	line "art is hung in a"
+	
+	para "museum for all to"
+	line "see too."
+	done
+
+GoldenrodMuseum2FSightseerMNoPaintingText:
+	text "Aww…! Smeargle and"
+	line "I came from Ecru-"
+
+	para "teak City because"
+	line "we heard an art"
+
+	para "exhibit would be"
+	line "here for the grand"
+	cont "opening."
+
+	para "But it seems we"
+	line "heard wrong…"
+
+	para "All the displays"
+	line "are empty!"
+	done
+
+GoldenrodMuseum2FSmeargleText:
+	text "Smeargle: Smeer!"
 	done
