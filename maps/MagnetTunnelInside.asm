d39187bbecf20948549ba35a9d94ebe9fc790fc5
The Void <9028176+etdv-thevoid@users.noreply.github.com>
2022-07-16 12:10:06 -0500

    Squashed commit of the following:
    commit 4bce1c089a860ee7c6180f73aad50a668c798c8e
    Author: The Void <9028176+etdv-thevoid@users.noreply.github.com>
    Date:   Fri Jul 15 14:01:03 2022 -0500
    
        finalize wild mon data
    
    commit 1c88542473384c50949f72154fca629f5a7e9738
    Author: The Void <9028176+etdv-thevoid@users.noreply.github.com>
    Date:   Fri Jul 15 13:17:13 2022 -0500
    
        Finalize NPC dialog and add hidden item at the bottom of the pit
    
    commit f9b63e8aa7e88dac8c9f25007149c25100698741
    Author: The Void <9028176+etdv-thevoid@users.noreply.github.com>
    Date:   Fri Jul 15 09:21:23 2022 -0500
    
        Stub out more map features
    
        hidden item + a second non-trainer engineer npc
    
    commit 6c3f5492c2b7276b65a25353ce03088d83b8e81c
    Author: The Void <9028176+etdv-thevoid@users.noreply.github.com>
    Date:   Fri Jul 15 09:20:19 2022 -0500
    
        Reduce voltorb, raise magnemite
    
    commit 83cb417769880b884297e4612969942b6f9a8585
    Author: etdv-thevoid <9028176+etdv-thevoid@users.noreply.github.com>
    Date:   Fri Jul 15 00:15:03 2022 -0500
    
        swap duplicate misdreavous move that was moved because duplicate
    
    commit 584c425497a36e6c6865427b6718dd7562a15c8b
    Author: etdv-thevoid <9028176+etdv-thevoid@users.noreply.github.com>
    Date:   Fri Jul 15 00:08:43 2022 -0500
    
        Rearrange wild data + fix text error
    
    commit 90bdff6540ddf32d7b6e2428ed30f148db3e456e
    Author: etdv-thevoid <9028176+etdv-thevoid@users.noreply.github.com>
    Date:   Thu Jul 14 23:52:24 2022 -0500
    
        Open up cave entrances
    
    commit 83d38b392da34525e3c96a44721168119acc7db3
    Author: The Void <9028176+etdv-thevoid@users.noreply.github.com>
    Date:   Thu Jul 14 23:32:24 2022 -0500
    
        Give Magnemite unique wild move
    
    commit 9d5eca46dc66de3804e86e34bd33a34f8bc04c1a
    Author: The Void <9028176+etdv-thevoid@users.noreply.github.com>
    Date:   Thu Jul 14 23:06:52 2022 -0500
    
        Add NPC dialog
    
    commit 9ba108d19eee11d03d0292f7d8fc4b9ff800e272
    Author: The Void <9028176+etdv-thevoid@users.noreply.github.com>
    Date:   Thu Jul 14 19:26:00 2022 -0500
    
        fix bad map constant
    
    commit 99f90817ae565b719b4be3f69bdf62ec8f1d6a1a
    Author: etdv-thevoid <9028176+etdv-thevoid@users.noreply.github.com>
    Date:   Thu Jul 14 18:20:58 2022 -0500
    
        Add NPCs and Warps to map
    
    commit fc599100c83080c736702457923f66369db2f840
    Author: The Void <9028176+etdv-thevoid@users.noreply.github.com>
    Date:   Thu Jul 14 15:22:30 2022 -0500
    
        MOR VOLTORB
    
    commit 3ecdf0b91136229fbff2e8dc5d31028263e72c82
    Author: The Void <9028176+etdv-thevoid@users.noreply.github.com>
    Date:   Thu Jul 14 15:08:01 2022 -0500
    
        Geodude goes BOOM
    
    commit 22084e13c904b2ad9480a177a6553bdcf3ea91a1
    Author: The Void <9028176+etdv-thevoid@users.noreply.github.com>
    Date:   Thu Jul 14 14:47:50 2022 -0500
    
        Change names
    
    commit 6539c3771f3b698af5e7ac016bcadbc833b4a5dc
    Author: The Void <9028176+etdv-thevoid@users.noreply.github.com>
    Date:   Thu Jul 14 14:35:37 2022 -0500
    
        Stub out trainers and TM for gyro ball
    
    commit 97b2d17665cabdbc4a1bb9d32a379c038741c158
    Author: The Void <9028176+etdv-thevoid@users.noreply.github.com>
    Date:   Thu Jul 14 14:34:49 2022 -0500
    
        Add wild encounters for Magnet Tunnel

--- maps/MagnetTunnelInside.asm
+++ maps/MagnetTunnelInside.asm
@@ -7,6 +7,10 @@ MagnetTunnelInside_MapScriptHeader:
 	def_warp_events
 	warp_event  5, 19, MAGNET_TUNNEL_WEST, 1
 	warp_event 43,  3, MAGNET_TUNNEL_EAST, 1
+	warp_event 27,  3, MAGNET_TUNNEL_INSIDE, 4
+	warp_event 27, 31, MAGNET_TUNNEL_INSIDE, 3
+	warp_event 34,  5, MAGNET_TUNNEL_INSIDE, 6
+	warp_event 34, 33, MAGNET_TUNNEL_INSIDE, 5
 
 	def_coord_events
 
@@ -19,12 +23,19 @@ MagnetTunnelInside_MapScriptHeader:
 	bg_event 21,  8, BGEVENT_UP, MagnetTunnelInsideMachine3
 	bg_event 34,  9, BGEVENT_UP, MagnetTunnelInsideMachine4
 	bg_event 35,  9, BGEVENT_UP, MagnetTunnelInsideMachine4
+	bg_event 38, 30, BGEVENT_ITEM + METAL_POWDER, EVENT_MAGNET_TUNNEL_HIDDEN_METAL_POWDER
 
 	def_object_events
 	object_event  3, 10, SPRITE_N64, SPRITEMOVEDATA_STANDING_LEFT, 0, 0, -1, -1, PAL_NPC_ROCK, OBJECTTYPE_COMMAND, jumptext, MagnetTunnelInsideLodestoneText, EVENT_MAGNET_TUNNEL_LODESTONE_1
 	object_event 20, 23, SPRITE_N64, SPRITEMOVEDATA_STANDING_LEFT, 0, 0, -1, -1, PAL_NPC_ROCK, OBJECTTYPE_COMMAND, jumptext, MagnetTunnelInsideLodestoneText, EVENT_MAGNET_TUNNEL_LODESTONE_2
 	object_event 20,  5, SPRITE_N64, SPRITEMOVEDATA_STANDING_LEFT, 0, 0, -1, -1, PAL_NPC_ROCK, OBJECTTYPE_COMMAND, jumptext, MagnetTunnelInsideLodestoneText, EVENT_MAGNET_TUNNEL_LODESTONE_3
 	object_event 31,  8, SPRITE_N64, SPRITEMOVEDATA_STANDING_LEFT, 0, 0, -1, -1, PAL_NPC_ROCK, OBJECTTYPE_COMMAND, jumptext, MagnetTunnelInsideLodestoneText, EVENT_MAGNET_TUNNEL_LODESTONE_4
+	object_event  8, 13, SPRITE_ENGINEER, SPRITEMOVEDATA_WANDER, 1, 1, -1, -1, 0, OBJECTTYPE_COMMAND, jumptextfaceplayer, MagnetTunnelInsideEngineerText1, -1
+	object_event 32, 21, SPRITE_ENGINEER, SPRITEMOVEDATA_WANDER, 1, 1, -1, -1, 0, OBJECTTYPE_COMMAND, jumptextfaceplayer, MagnetTunnelInsideEngineerText2, -1
+	object_event 22,  8, SPRITE_ENGINEER, SPRITEMOVEDATA_STANDING_DOWN, 0, 0, -1, -1, 0, OBJECTTYPE_GENERICTRAINER, 2, GenericTrainerEngineerGraden, -1
+	object_event 31, 12, SPRITE_ENGINEER, SPRITEMOVEDATA_STANDING_RIGHT, 0, 0, -1, -1, 0, OBJECTTYPE_GENERICTRAINER, 4, GenericTrainerEngineerKevin, -1
+	object_event 24, 23, SPRITE_ENGINEER, SPRITEMOVEDATA_STANDING_UP, 0, 0, -1, -1, 0, OBJECTTYPE_GENERICTRAINER, 3, GenericTrainerEngineerCameron, -1
+	tmhmball_event 19, 21, TM_GYRO_BALL, EVENT_MAGNET_TUNNEL_TM_GYRO_BALL
 
 	object_const_def
 	const MAGNETTUNNELINSIDE_STONE_1
@@ -190,7 +201,8 @@ MagnetTunnelInsideMachine4:
 
 .MachineOn:
 	checkevent EVENT_MAGNET_TUNNEL_LODESTONE_IN_PIT
-	iftrue_jumptext MagnetTunnelInsideMachineText0
+	iftrue_jumptext MagnetTunnelInsideMachineText6
+	scall MagnetTunnelAlarmSounds
 	opentext
 	writetext MagnetTunnelInsideMachineText4
 	yesorno
@@ -222,21 +234,16 @@ MagnetTunnelInsideMachine4:
 	remove_fixed_facing
 	step_end
 
-MagnetTunnelInsideMachineText0:
-	text "The machine is"
-	line "off."
-	done
-
 MagnetTunnelInsideMachineText1:
 	text "The machine is"
-	line "off."
+	line "powered off."
 
 	para "Turn it on?"
 	done
 
 MagnetTunnelInsideMachineText2:
 	text "Ping! The machine"
-	line "turned on."
+	line "powered on."
 	done
 
 MagnetTunnelInsideMachineText3:
@@ -246,15 +253,43 @@ MagnetTunnelInsideMachineText3:
 
 MagnetTunnelInsideMachineText4:
 	text "The machine is"
-	line "running smoothly."
+	line "running quite"
+	cont "erratically."
 
-	para "Turn it off?"
+	para "Multiple WARNING!"
+	line "messages continue"
+	
+	para "to rapidly flash"
+	line "across the screen."
+
+	para "Hit the emergency"
+	line "shutdown button?"
 	done
 
 MagnetTunnelInsideMachineText5:
-	text "Ping! The machine"
-	line "turned off."
+	text "Ding! The machine"
+	line "powered off."
 	done
+
+MagnetTunnelInsideMachineText6:
+	text "The machine is"
+	line "surrounded by the"
+	
+	para "faint stench of"
+	line "melted plastic."
+
+	para "Better be safe and"
+	line "not try to turn it"
+	cont "back on."
+	done
+
+MagnetTunnelAlarmSounds:
+	playsound SFX_LICK
+	pause 10
+	playsound SFX_LICK
+	pause 10
+	playsound SFX_LICK
+	end
 
 MagnetTunnelInsideLodestoneText:
 	text "The lodestone is"
@@ -262,4 +297,170 @@ MagnetTunnelInsideLodestoneText:
 
 	para "It has a faint"
 	line "aura of static."
+	done
+
+MagnetTunnelInsideEngineerText1:
+	text "The lodestone here"
+	line "gives off a strong"
+	cont "magnetic field."
+
+	para "We had to bring in"
+	line "specialized equip-"
+
+	para "ment to complete"
+	line "this tunnel by the"
+	cont "foreman's deadline."
+
+	para "Our team was under"
+	line "heavy pressure from"
+
+	para "the general public"
+	line "to deliver on time."
+	done
+
+MagnetTunnelInsideEngineerText2:
+	text "Ever since we"
+	line "began construction"
+
+	para "on this tunnel,"
+	line "the number of wild"
+
+	para "Electric-type"
+	line "#mon has more"
+
+	para "than doubled with"
+	line "the passing of"
+	cont "each day."
+
+	para "The foreman thinks"
+	line "they must all be"
+
+	para "attracted to the"
+	line "lodestone's unique"
+	cont "energy field."
+	done
+
+GenericTrainerEngineerGraden:
+	generictrainer ENGINEER, ENGINEER_GRADEN, EVENT_BEAT_ENGINEER_GRADEN, .SeenText, .BeatenText
+
+	text "I'm the foreman"
+	line "on this jobsite."
+
+	para "We dug out this"
+	line "tunnel to make a"
+	
+	para "path for the new"
+	line "Magnet Train"
+	cont "railway."
+
+	para "Haha! Little did I"
+	line "know all of the"
+
+	para "lodestone would"
+	line "cause us so much"
+	cont "headache!"
+
+	para "Everyday, more and"
+	line "more Electric-type"
+
+	para "#mon show up"
+	line "and cause delays."
+
+	para "We thought this"
+	line "tunnel was for the"
+	
+	para "Magnet Train, but"
+	line "it turned out to"
+	
+	para "be more like a"
+	line "home for magnet"
+	cont "#mon! Hahaha!"
+	done
+
+.SeenText:
+	text "Admiring all of"
+	line "our handiwork?"
+
+	para "Haha!"
+	line "Admire this!"
+	done
+
+.BeatenText:
+	text "Haha! You've got"
+	line "guts kid."
+	done
+
+GenericTrainerEngineerKevin:
+	generictrainer ENGINEER, ENGINEER_KEVIN, EVENT_BEAT_ENGINEER_KEVIN, .SeenText, .BeatenText
+
+	para "This machine has"
+	line "been acting up as"
+	cont "of late."
+
+	para "I've tried to fix"
+	line "it, but its been"
+	cont "no use…"
+
+	para "I'm worried if I"
+	line "turn it on again"
+
+	para "it will short"
+	line "circuit and be"
+	cont "kaput…"
+
+	para "Don't tell the"
+	line "foreman, okay?"
+
+	para "And don't mess with"
+	line "it either! Deal?"
+	done
+
+.SeenText:
+	text "Stand back!"
+
+	para "This machine has"
+	line "been on the fritz!"
+	done
+
+.BeatenText:
+	text "Blown out! Just"
+	line "like a fuse…"
+	done
+
+GenericTrainerEngineerCameron:
+	generictrainer ENGINEER, ENGINEER_CAMERON, EVENT_BEAT_ENGINEER_CAMERON, .SeenText, .BeatenText
+
+	text "These things can"
+	line "generate some"
+	cont "serious power."
+
+	para "When powered on,"
+	line "they generate a"
+
+	para "pretty strong"
+	line "magnetic field."
+
+	para "That sure comes in"
+	line "handy when moving"
+
+	para "large, heavy, and"
+	line "magnetic rocks"
+	cont "like lodestone."
+
+	para "But they can be"
+	line "real dangerous to"
+	cont "operate."
+
+	para "So be careful in"
+	line "here, alright?"
+	done
+
+.SeenText:
+	text "Hey! Be careful"
+	line "around the"
+	cont "machinery kid!"
+	done
+
+.BeatenText:
+	text "Shutdown!"
 	done
