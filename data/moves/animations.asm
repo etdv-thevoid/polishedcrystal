ed5ee8e7a4c92d0a9e6494baf821da17a9221cf7
The Void <9028176+etdv-thevoid@users.noreply.github.com>
2022-07-17 13:57:35 -0500

    update dizzy punch anim

--- data/moves/animations.asm
+++ data/moves/animations.asm
@@ -2154,7 +2154,9 @@ BattleAnim_MindReader: ; removed
 BattleAnim_DizzyPunch:
 	anim_2gfx ANIM_GFX_STATUS, ANIM_GFX_HIT
 	anim_sound 0, 1, SFX_COMET_PUNCH
-	anim_obj ANIM_OBJ_PUNCH, -15, 0,  7, 0, $0
+	anim_obj ANIM_OBJ_PUNCH, -15, 0,   7, 0, $0
+	anim_wait 6
+	anim_obj ANIM_OBJ_HIT_YFIX, -15, 0,   7, 0, $0
 	anim_wait 16
 	anim_sound 0, 1, SFX_KINESIS
 	anim_obj ANIM_OBJ_CHICK, -15, 0,   3, 0, $15
