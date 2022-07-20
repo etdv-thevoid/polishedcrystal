b2723c660c2699915bf4f246ef2234129512f246
The Void <9028176+etdv-thevoid@users.noreply.github.com>
2022-07-20 10:54:19 -0500

    change some giga drain variables

--- data/moves/animations.asm
+++ data/moves/animations.asm
@@ -4230,12 +4230,12 @@ BattleAnim_GigaDrain:
 	anim_jumpvar $1, .loop
 	anim_jumpvar $2, .loop
 	anim_sound 6, 3, SFX_WATER_GUN
-	anim_obj ANIM_OBJ_ABSORB, -16, 0,   7, 0, $3
+	anim_obj ANIM_OBJ_ABSORB, -16, 0,   10, 0, $5
 	anim_wait 6
 	anim_sound 6, 3, SFX_WATER_GUN
-	anim_obj ANIM_OBJ_ABSORB, -16, 0,   5, 0, $4
+	anim_obj ANIM_OBJ_ABSORB, -16, 0,   2, 0, $6
 	anim_wait 6
-	anim_jumpvar $3, .spawn
+	anim_jumpvar $4, .spawn
 	anim_jumpvar $8, .done
 	anim_jump .loop
 .spawn
