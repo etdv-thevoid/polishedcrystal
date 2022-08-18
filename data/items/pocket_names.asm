6c97b0f5e05809d714f88c16bd805f9fd9c23954
The Void <9028176+etdv-thevoid@users.noreply.github.com>
2022-08-18 08:43:16 -0500

    revert pack changes

--- data/items/pocket_names.asm
+++ data/items/pocket_names.asm
@@ -3,8 +3,8 @@ ItemPocketNames:
 	dw .Item
 	dw .Medicine
 	dw .Ball
-	dw .Berry
 	dw .TM ; impossible
+	dw .Berry
 	dw .Key ; impossible
 	assert_table_length NUM_POCKETS
 
@@ -14,9 +14,9 @@ ItemPocketNames:
 	db "Med.Pocket@"
 .Ball:
 	db "Ball Pocket@"
-.Berry:
-	db "Berry Pocket@"
 .TM:
 	db "TM Pocket@"
+.Berry:
+	db "Berry Pocket@"
 .Key:
 	db "Key Pocket@"
